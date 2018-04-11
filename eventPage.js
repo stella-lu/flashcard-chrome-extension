725877bdcdf1e3fb2cf16b52e0d0f051f0064f77
Stella Lu <stella-lu@outlook.com>
2018-04-11 14:42:38 -0700

    Display Alert On Click

--- eventPage.js
+++ eventPage.js
@@ -2,5 +2,10 @@ var contextMenuItem = {
     "id": "flashcards",
     "title": "Flashcards",
     "contexts": ["selection"]
+    
 }
 chrome.contextMenus.create(contextMenuItem)
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    alert("hallo");
+});
