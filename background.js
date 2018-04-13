7549d668f1e236c43aaeca89a9bf50897967bab8
Stella Lu <stella-lu@outlook.com>
2018-04-13 15:48:31 -0700

    Center Menu Icons and Rename Background Javascript File

--- background.js
+++ background.js
@@ -6,6 +6,16 @@ var contextMenuItem = {
 }
 chrome.contextMenus.create(contextMenuItem)
 
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     alert("hallo");
 });
+
+
+/*
+chrome.contextMenus.create({
+	title: 'Add "%s" to Flashcards',
+	contexts: ['selection'],
+	onclick: alert("hallo"),
+});
+*/
