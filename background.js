20af4b9163c0d23c0b5aa5b868dfbce35eff03cc
Carson Trinh <carsontrinh@gmail.com>
2018-04-15 10:18:32 -0700

    Figured out how to extra text from selection.

--- background.js
+++ background.js
@@ -2,20 +2,11 @@ var contextMenuItem = {
     "id": "flashcards",
     "title": "Flashcards",
     "contexts": ["selection"]
-    
 }
+
 chrome.contextMenus.create(contextMenuItem)
 
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    alert("hallo");
+    alert(info.selectionText);
 });
-
-
-/*
-chrome.contextMenus.create({
-	title: 'Add "%s" to Flashcards',
-	contexts: ['selection'],
-	onclick: alert("hallo"),
-});
-*/
