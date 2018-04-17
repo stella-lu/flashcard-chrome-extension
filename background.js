7bd779bdd0cfb0cf99dd1ab7f63ee0e06435d090
Dmytro Khelemendyk <dkhelemendyk@berkeley.edu>
2018-04-17 12:09:52 -0700

    changed event handling and DB logic, tested existing implementation

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-//db name
-const dbName = 'dbFlashCard'
-
 //create context menu item
 var contextMenuItem = {
     "id": "flashcards",
@@ -14,8 +11,11 @@ chrome.contextMenus.create(contextMenuItem)
 //attach event listener to the context menu item;
 //get selected text
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    //if "flashcards" button is clicked
     if (info.menuItemId) {
         var selectedText = info.selectionText;
+        console.log("selected text is " + selectedText);
+        processText(selectedText);
     } 
 });
 
@@ -23,9 +23,9 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 var button = document.getElementById("button");
 
 //add comment
-button.onclick = function() {
-	alert("you clicked a button!")
-}
+//button.onclick = function() {
+//	alert("you clicked a button!")
+//}
 
 //add comment
 function shuffle() {
@@ -33,53 +33,111 @@ function shuffle() {
 };
 
 
-//Open the database (this is a request); returns an instance of IDBDatabase
-//1 is a version of the current db, should be upgraded with each change
-var dbRequest = window.indexedDB.open(dbName, 1);
+/*--------------------------------DB Logic-----------------------------------------------*/
+var db;
+//db name
+const dbName = 'dbFlashCard';
 
-//create event handlers for the request above
-//what if user denies request to create a DB?
-//Note: IndexedDB storage for incognito sessions only exists in memory until
-//the incognito session is closed
-dbRequest.onerror = function(event) {
-    alert("Database open error!");
-    alert(event.$log)
-};
-
-//called when we change the db version (aka change our db, change db schema)
-request.onupgradeneeded = function (event) {
-    console.log("running onupgradeneeded");
-    var db = event.target.result;
+function processText(textHighlighted) {
+    console.log("inside process text");
+    /* Open the database (this is a request); returns an instance of IDBDatabase 1 is a version of the current db, should be upgraded with each change*/
+    var dbRequest = window.indexedDB.open(dbName, 1);
     
-    //add generic error handler to the DB instance to handle all possible errors; 
-    //without it, it would require adding error handlers for each request
-    db.onerror = function(event) {
-        alert("Database error: " + event.target.errorCode);
+    /* create event handlers for the request above
+    what if user denies request to create a DB?
+    Note: IndexedDB storage for incognito sessions only exists in memory until
+    the incognito session is closed */
+    dbRequest.onerror = function(event) {
+        alert("Database open error!");
+        alert(event.$log)
     };
-
-    // Create an objectStore (like a "table") to hold information about flashcards. 
-    // We use field "word" as the key path because we will make sure that the words stored
-    //in the database do not have duplicates
-    if(!db.objectStoreNames.contains("flashcards")) {
-			console.log("I am creating new objectstore for flashcards");
-			var objectStore = db.createObjectStore("flashcards", { keyPath: "word" });  
-			// Create an index to store the meaning of words; we might have duplicates for meaning
-            objectStore.createIndex("meaning", "meaining", { unique: false }); 
-		}       
-};
-
-//onsuccess is called each time we make a new request: even if the database schemas has not been changed
-request.onsuccess = function(event) {
-	db = event.target.result;
-	
-	db.onerror = function(event) {
-	    // Generic error handler for all errors targeted at this database's
-        // requests!
+    
+    //called when we change the db version (aka change our db, change db schema)
+    dbRequest.onupgradeneeded = function (event) {
+        console.log("running onupgradeneeded");
+        db = event.target.result;
+        //OR alternatively:
+        //db = dbRequest.result;
+        
+        /* add generic error handler to the DB instance to handle all possible errors; without it, it would require adding error handlers for each request */
+        db.onerror = function(event) {
         alert("Database error: " + event.target.errorCode);
-	};
+        };
+    
+        /* Create an objectStore (like a "table") to hold information about flashcards. 
+         We use field "word" as the key path because we will make sure that the words stored
+         in the database do not have duplicates */
+        if(!db.objectStoreNames.contains("flashcards")) {
+            console.log("I am creating new objectstore for flashcards");
+            var objectStore = db.createObjectStore("flashcards", { keyPath: "word" });  
+            // Create an index to store the meaning of words; we might have duplicates for meaning
+            objectStore.createIndex("meaning", "meaning", { unique: false }); 
+        }   
+    };
+    
+    /* onsuccess is called each time we make a new request: even if the database schemas has not been changed */
+    dbRequest.onsuccess = function(event) {
+        console.log("running onsuccess");
+        db = event.target.result;
+        //OR alternatively:
+        //db = dbRequest.result;
 
-    console.log("We have a handle to our indexeddb");
-};
+        /* Generic error handler for all errors targeted at this database's requests! */
+        db.onerror = function(event) {
+            alert("Database error: " + event.target.errorCode);
+            console.log("Database error " + event.target);
+        };
+        
+        //before we start a transaction, parse highlighted text
+        var textParsed = parseText(textHighlighted);
+
+        //start a read/write transaction
+        var transaction = db.transaction("flashcards", "readwrite");
+        //get object store from transaction
+        var store = transaction.objectStore("flashcards");
+        //check our highlighted text exists in the DB as a key
+        var req = store.openCursor(textParsed);
+        req.onsuccess = function(event) {
+            var cursor = event.target.target;
+            if (cursor) { // textParsed already exist
+                alert("word already exists in DB");
+                //try to get the parsed text's meaning from object store if we need to show it tot the user
+                //var wordMeaning = store.get(textParsed);
+                //cursor.update(textParsed);
+            } else { // textParsed not exist
+                alert("word is not in DB");
+                //find meaning of the word and add to the DB
+                //var meaning = findWordMeaning();
+                //store.put({wordToCheck, meaning}); 
+                //store.add(textParsed)
+            }
+        };        
+        
+        // report on the success of opening the transaction
+        transaction.oncomplete = function(event) {
+            console.log("transaction completed.")
+            db.close();
+        };
+
+        transaction.onerror = function(event) {
+            note.innerHTML += '<li>Transaction not opened due to error.</li>';
+        };
+
+    };
+}
+
+//text parser
+function parseText(text) {
+    console.log("inside text parser");
+    var textProcessed = "";
+    return textProcessed;
+}
+
+
+
+
+
+
 
 
 
